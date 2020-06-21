15eacf0d51e43faf6e0ab3afdb8c8d1fccb073e7
didiercruz <didiercruz@192.168.0.15>
2020-06-21 16:03:44 -0600

    List of thigs done: -Welcome Screen. -Domain Screen. -Mesh Screen. -Connectivity Table and Elements Screen. -Model Screen. -Implemented logic of transitions between Screens.

---
+++ src/practicai/welcome/FXMLWelcomeController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicai.welcome;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author didiercruz
+ */
+public class FXMLWelcomeController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private StackPane parentContainer;
+    @FXML
+    private AnchorPane anchorRoot;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loadDomainScreen(ActionEvent event) throws IOException {
+      Parent root = FXMLLoader.load(getClass().getResource("/practicai/domain/FXMLDomain.fxml"));
+      Scene scene = button.getScene();
+      
+      root.translateYProperty().set(scene.getHeight());
+      parentContainer.getChildren().add(root);
+      
+      Timeline timeline = new Timeline();
+      KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+      KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+      timeline.getKeyFrames().add(kf);
+      timeline.setOnFinished(event1->{
+          parentContainer.getChildren().remove(anchorRoot);
+      });
+      timeline.play();
+    }
+    
+}
